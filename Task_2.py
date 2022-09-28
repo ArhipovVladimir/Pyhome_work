10e6234880703efedf2a5fe90d0def7a9a5c9e16
ArhipovVS <Arhipof05@yandex.ru>
2022-09-28 23:06:32 +0700

    predik

--- Task_2.py
+++ Task_2.py
@@ -2,4 +2,6 @@
 # утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для
 #  всех значений предикат.
 
-if  not (x or y or z) == not (x) and not (y) and not (z)
+# вообще не пониаю задание
+
+#if  not (x or y or z) == not (x) and not (y) and not (z)
