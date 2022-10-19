912d6bf3a9f67a1cd7193dd201c4db14fe9fdee8
ArhipovVS <Arhipof05@yandex.ru>
2022-10-19 22:49:12 +0700

    111

--- Task3_4.py
+++ Task3_4.py
@@ -8,12 +8,11 @@
 # 5.Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
 def dex_bin (num):
-    result = str('z')
-    while num >0:
-        resalt = str(num % 2) + resalt
+    result = " "
+    while num > 0:
+        resalt = str(num % 2) + resalt 
         num = num // 2
     return result
-
 
 
 dig=int(input("введите число: "))
