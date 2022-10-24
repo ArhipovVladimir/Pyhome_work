b1b6c1ac5523a1e66adbfeae12d30edeaf995e23
ArhipovVS <Arhipof05@yandex.ru>
2022-10-24 23:18:23 +0700

    Task4_1

---
+++ Seminar4/Task4_1.py
@@ -0,0 +1,14 @@
+# 1 Вычислить число c заданной точностью d
+# Пример:
+# при d = 0.001, π = 3.141 10-1 <= d <= 10-10
+import math
+
+
+def res_round (r):
+    r1 = len(r.split('.')[1])
+    resalt = round (math.pi,r1)
+    return resalt
+
+
+razm = input('Веедите точность числа Например 0.0001')
+print (res_round (razm))
