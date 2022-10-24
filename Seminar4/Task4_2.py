0547652923e9c96d0f518fe9fe23d306c84fc5e5
ArhipovVS <Arhipof05@yandex.ru>
2022-10-25 00:46:38 +0700

    Task4_2

--- Seminar4/Task4_2.py
+++ Seminar4/Task4_2.py
@@ -1,2 +1,13 @@
 # 2 Задайте натуральное число N. Напишите программу, которая составит список
 # простых множителей числа N.
+
+def find_mult (dig1):
+    num = dig1
+    spisok = []
+    while num != 1: 
+        for i in (2, 3, 5, 7, 11, 13, 17):
+            if num % i == 0:spisok.append(i); num = num / i
+    return spisok
+
+dig = int(input("введите число: "))
+print (find_mult (dig))
