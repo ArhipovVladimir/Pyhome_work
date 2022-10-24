778b6aa19aeb2ce5d95b0fc4b8cb7b131a31f736
ArhipovVS <Arhipof05@yandex.ru>
2022-10-25 01:27:53 +0700

    Task4_4

--- Seminar4/Task4_4.py
+++ Seminar4/Task4_4.py
@@ -3,3 +3,17 @@
 # степени k.
 # Пример:
 # k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+import random
+
+def get_urovn(k):
+    resalt =""
+    for i in range (k, 0 , -1):
+        resalt = resalt + str(random.randint(0, 101)) + "x^" + str(i) + "+" 
+    resalt = resalt + str(random.randint(0, 101))
+    return resalt
+
+
+pow = int(input("степень: "))
+urov = get_urovn(pow)
+print (urov)
