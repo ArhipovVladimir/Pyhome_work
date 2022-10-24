abecea3f7181216090cc6e474381685178641e4c
ArhipovVS <Arhipof05@yandex.ru>
2022-10-25 00:52:02 +0700

    Task4_2

--- Seminar4/Task4_2.py
+++ Seminar4/Task4_2.py
@@ -2,10 +2,11 @@
 # простых множителей числа N.
 
 def find_mult (dig1):
+    prost_dig = [2, 3, 5, 7, 11, 13, 17]
     num = dig1
     spisok = []
     while num != 1: 
-        for i in (2, 3, 5, 7, 11, 13, 17):
+        for i in prost_dig:
             if num % i == 0:spisok.append(i); num = num / i
     return spisok
 
