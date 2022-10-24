5f2e78967da8e0d700f4ab54ddb0bde68ba99f5a
ArhipovVS <Arhipof05@yandex.ru>
2022-10-24 23:37:37 +0700

    Task4_3

--- Seminar4/Task4_3.py
+++ Seminar4/Task4_3.py
@@ -1,2 +1,15 @@
 # 3 Задайте последовательность чисел. Напишите программу, которая выведет список
 # неповторяющихся элементов исходной последовательности.
+
+
+
+def count1 (lister):
+    new_list = []
+    for i in lister:
+        counter = lister.count(i)
+        if counter == 1: new_list.append(i) 
+    return new_list
+
+list1 = [2, 3, 1, 5 , 1]
+print (list1)
+print (count1(list1))
