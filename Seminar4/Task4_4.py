e3831c7510c4feeb83d18293e54e7f07ce95b402
ArhipovVS <Arhipof05@yandex.ru>
2022-10-30 19:53:45 +0700

    Task5_2

--- Seminar4/Task4_4.py
+++ Seminar4/Task4_4.py
@@ -20,4 +20,38 @@ print (urov)
 
 data = open ('file.txt', 'w') 
 data.write (urov)
-data.close
+data.close()
+
+
+# 4 Р—Р°РґР°РЅР° РЅР°С‚СѓСЂР°Р»СЊРЅР°СЏ СЃС‚РµРїРµРЅСЊ k. РЎС„РѕСЂРјРёСЂРѕРІР°С‚СЊ СЃР»СѓС‡Р°Р№РЅС‹Рј РѕР±СЂР°Р·РѕРј СЃРїРёСЃРѕРє
+# РєРѕСЌС„С„РёС†РёРµРЅС‚РѕРІ (Р·РЅР°С‡РµРЅРёСЏ РѕС‚ 0 РґРѕ 100) РјРЅРѕРіРѕС‡Р»РµРЅР° Рё Р·Р°РїРёСЃР°С‚СЊ РІ С„Р°Р№Р» РјРЅРѕРіРѕС‡Р»РµРЅ
+# СЃС‚РµРїРµРЅРё k.
+# РџСЂРёРјРµСЂ:
+# k=2 => 2xВІ + 4x + 5 = 0 РёР»Рё xВІ + 5 = 0 РёР»Рё 10*xВІ = 0
+
+import random
+
+
+def get_urovn(k):
+    resalt = ""
+    for i in range(k, 0 , -1):
+        resalt = resalt + str(random.randint(0, 101)) + " x^" + str(i) + " + " 
+    resalt = resalt + str(random.randint(0, 101)) + " = 0"
+    return resalt
+
+
+pow = int(input("СЃС‚РµРїРµРЅСЊ: "))
+urov = get_urovn(pow)
+print(urov)
+
+data = open('file.txt', 'w')
+data.write(urov)
+data.close()
+
+
+# pow - СЌС‚Рѕ РІСЃС‚СЂРѕРµРЅРЅР°СЏ С„СѓРЅРєС†РёСЏ! Р›СѓС‡С€Рµ РїРµСЂРµРёРјРµРЅРѕРІР°С‚СЊ РїРµСЂРµРјРµРЅРЅСѓСЋ!
+# pow -> input_pow
+# Р»СѓС‡С€Рµ РёСЃРїРѕР»СЊР·РѕРІР°С‚СЊ РЅРµ С‚СЂР°РЅСЃР»РёС‚РµСЂР°С†РёСЋ, Р° Р°РЅРіР»РёР№СЃРєРёРµ СЃР»РѕРІР°:
+# resalt -> result
+# urov -> level
+# get_urovn -> get_level
--- Seminar5/Task5_2.py
+++ Seminar5/Task5_2.py
@@ -17,20 +17,21 @@
 # {'дом': 2, 'окно': 2, 'дверь': 2, 'стена': 1, 'кухня': 1, 'стол': 2, 'стул': 3}
 
 stroka = 'дом, окно, дверь, стена, кухня, стол, стул, дверь, дом, стул, стол, окно, стул'
-
 print(stroka)
 
-list_stroka=list(stroka)
+list_stroka=stroka.split(", ")
 print(list_stroka)
 
 set_stroka=set(list_stroka)
+print(set_stroka)
+
 list_srtoka_no_dubl=list(set_stroka)
 list_count=[]
+
+# list_count = map(lambda element: list_count.append(list_stroka.count(element), list_stroka)) 
 for element in list_srtoka_no_dubl:
     list_count.append(list_stroka.count(element))
 
 result_dict = dict(zip(list_srtoka_no_dubl, list_count))
 print (result_dict)
 
-# 3 Дан файл jack.txt (https://disk.yandex.ru/d/orFlUSXkcA600w)
-# по аналогии с предыдущим заданием составить аналогичный словарь.
